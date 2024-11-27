0bab8292d9bdf118c4eb3b72cf26f16ec6e63544
nainu <ammadanwar416@gmail.com>
2024-11-27 21:40:34 +0500

    4th

--- client/src/components/TextEditor.js
+++ client/src/components/TextEditor.js
@@ -1,10 +1,23 @@
-import React, { useEffect , useRef} from 'react'
+import React from 'react'
 import Quill from 'quill'
 import "quill/dist/quill.snow.css"
 
 
 // 
 export default function TextEditor() {
+    // adding options in the toolbar
+    const toolbar_options = React.useMemo(() => [
+        [{ header : [1,2,3,4,5,6,false]} ],
+        [{ font : [] }], // out theme has default fonts
+        [{ list : "ordered"} , { list : "bulled"}],
+        ["bold" , "italic" , "underline"],
+        [{ color : []}, { background : [] }],
+        [{ script : "sub"} , { script : "super"}],
+        [{ align : []}],
+        [ "image" , "blockquote" , "code-block"],
+        ["clean"]
+    ], []);
+
     // Quill is not directly compatible with the react hence we make a container name id of a div
     // and then we make a Quill object
     // useEffect is used to run and it should run only once
@@ -17,10 +30,12 @@ export default function TextEditor() {
         const editor = document.createElement('div');
         wrapper.append(editor);
         // creating the Quill object with theme snow which is already imported
-        new Quill(editor , { theme: "snow" });
+        new Quill(editor , {
+            theme: "snow" ,
+            modules : {toolbar : toolbar_options} });
 
-    }, []);
-  return (
+    }, [toolbar_options]);
+    return (
     <div className='container' ref={wrapperRef}></div>
-  )
+    )
 }
